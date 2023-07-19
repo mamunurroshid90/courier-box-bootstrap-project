442c91fca9ce56ddc4be0e81427f9ac21676416b
mamunur-roshid <nijummamun7@gmail.com>
2023-07-20 00:07:24 +0600

    courier bootstrap project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".main__slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: true,
+  prevArrow: '<i class="fa-solid fa-angle-left left"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right right"></i>',
+});
